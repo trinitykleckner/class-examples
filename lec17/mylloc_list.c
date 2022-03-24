99c992a77fcaf38a7a468202efc3441b7aefee19
alinen <alinen@savvysine.com>
2022-03-24 14:27:55 -0400

    class example

--- lec17/mylloc_list.c
+++ lec17/mylloc_list.c
@@ -12,15 +12,23 @@ void *malloc (size_t size) {
   if (size == 0){
     return NULL;
   }
-  void *memory = sbrk(size);
+  // Step 1: check free list for memory 
+  // If found, remove it from free list, and return it
+
+  // Step 2: only allocated if no memory found
+  void *memory = sbrk(size + sizeof(struct chunk));
   if (memory == (void *) -1) {
     return NULL;
   } else {
-    return memory;
+    struct chunk* chunk = (struct chunk*) memory;
+    chunk->size = size;
+    chunk->next = NULL;
+    return (void*) (chunk + 1);
   }
 }
 
 void free(void *memory) {
+  // Add memory free list
   return;
 }
 
